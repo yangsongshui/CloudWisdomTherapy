18229c37dd01d1c4d77485d765ae20f4cfdfd123
yangsonshui <362927657@qq.com>
2017-01-12 17:43:57 +0800

    11.12

---
+++ app/src/main/java/aromatherapy/saiyi/cn/cloudwisdomtherapy/activity/RegisterActivity.java
@@ -0,0 +1,21 @@
+package aromatherapy.saiyi.cn.cloudwisdomtherapy.activity;
+
+import android.os.Bundle;
+
+import aromatherapy.saiyi.cn.cloudwisdomtherapy.R;
+import aromatherapy.saiyi.cn.cloudwisdomtherapy.bean.BaseActivity;
+
+public class RegisterActivity extends BaseActivity {
+
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+}
